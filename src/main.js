64c7b385cc2552f721da6dba79fc6ee58469522c
Sanches-23 <pasechnik23.2002@gmail.com>
2023-02-15 18:06:23 +0200

    1st steps

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
+
+import App from './App.vue'
+import router from './router'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+
+import './assets/main.css'
+
+const app = createApp(App)
+const pinia =createPinia()
+pinia.use(piniaPluginPersistedState)
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
+
