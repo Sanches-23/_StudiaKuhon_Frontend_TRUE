ec16adf6b56061f2b282125ba82eb2e1163f931e
Sanches-23 <pasechnik23.2002@gmail.com>
2023-02-20 16:30:35 +0200

    20.02.2023 not working product view

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,76 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container-fluid text-center p-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
+      <a class="navbar-brand" href="#">Studia Kuhon</a>
+      <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <RouterLink class="nav-item nav-link" to="/">Home</RouterLink>
+          <RouterLink class="nav-item nav-link" to="/products">products</RouterLink>
+          <RouterLink class="nav-item nav-link" to="/about">About us</RouterLink>
+          <RouterLink class="nav-item nav-link" to="/faq">F.A.Q</RouterLink>
+        </div>
+      </div>
+<!--      <wishlist></wishlist>-->
+<!--      <login-popup></login-popup>-->
+    </nav>
+   <RouterView />
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+<!--  <div class="bg-dark collapse" id="navbarHeader" style="">-->
+<!--    <div class="container">-->
+<!--      <div class="row">-->
+<!--        <div> &lt;!&ndash;class="col-sm-8 col-md-7 py-4"&ndash;&gt;-->
+<!--          <h4 class="text-white">Про нас</h4>-->
+<!--          <p class="text-muted">-->
+<!--            Компанія studk-->
+<!--          </p>-->
+<!--        </div>-->
+<!--        <div >&lt;!&ndash;class="col-sm-4 offset-md-1 py-4"&ndash;&gt;-->
+<!--          <h4 class="text-white">Contact</h4>-->
+<!--          <ul class="list-unsettled">-->
+<!--            <li><a href="#" class="text-white">Follow on Twitter</a></li>-->
+<!--            <li><a href="#" class="text-white">Like on Facebook</a></li>-->
+<!--            <li><a href="#" class="text-white">Email me</a></li>-->
+<!--          </ul>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--  <div class="navbar navbar-dark bg-dark shadow-sm">-->
+<!--    <div class="container">-->
+<!--      <a href="#" class="navbar-brand d-flex align-items-center">-->
+<!--&lt;!&ndash;        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"&ndash;&gt;-->
+<!--&lt;!&ndash;             stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2"&ndash;&gt;-->
+<!--&lt;!&ndash;             viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2&ndash;&gt;-->
+<!--&lt;!&ndash;             0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>&ndash;&gt;-->
+<!--        <strong>Album</strong>-->
+<!--      </a>-->
+<!--      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"-->
+<!--              aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--      </button>-->
+<!--    </div>-->
+<!--  </div>-->
 
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
+
+
